4f01efb3c30ee24ade43c91231f650b1e2211112
Peter <72198254+pbtodorov@users.noreply.github.com>
2020-12-13 12:56:13 +0200

    Update task4.cpp

--- Tasks/Week 10/Допълнително 1 група/Solutions/task4.cpp
+++ Tasks/Week 10/Допълнително 1 група/Solutions/task4.cpp
@@ -1,34 +1,34 @@
 #include <iostream>
 #include <string>
 using namespace std;
-const int N = 10;
+const int N = 10;//глобална константа; използваме я за заделяне на памет за масива
 
 
 int main()
 {
-int arr[N][N];
-	int n;
+int arr[N][N];//създаваме двумерен масив 10 на 10
+	int n;//числото, с което ще работим;
 	cin >> n;
 	int value = 1;
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < n; i++)//с този фор цикъл даваме стойности на елементите от масива
 	{
 		for (int j = 0; j < n; j++)
 		{
-			arr[i][j] = value++;
+			arr[i][j] = value++;//на i-ти ред и j-ти стълб елемента става колкото value, след което заради ++ value се увеличава
 		}
 	}
 
-	int copyJ;
-	for (int j = 0; j < n; j++)
+	int copyJ;//копие на j, за да не го променяме
+	for (int j = 0; j < n; j++)//за всяка колкона правим едно и също
 	{
 		copyJ = j;
-		for (int i = n - 1; i >= 0 && copyJ >=0; i--)
+		for (int i = n - 1; i >= 0 && copyJ >=0; i--)//от последния ред вървим нагоре
 		{
 			cout << arr[i][copyJ] << " ";
-			copyJ--;
+			copyJ--;//и наляво
 		}
 	}
-	int copyI;
+	int copyI;//тук аналогично същото правим като в предния цикъл
 	for (int i = n - 2; i >= 0; i--)
 	{
 		copyI = i;
